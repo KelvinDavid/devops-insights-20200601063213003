9be992be02b7ef1dd1d04c32e1aa1bfd7c90be51
Kelvin David <kdroomo416@gmail.com>
2020-06-03 14:20:36 +0000

    Attempting Encap

--- src/mapApp.js
+++ src/mapApp.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Helmet} from "react-helmet";
 import Map from './map.js'
 import './map.css'
 
-//function MapApp() {
-//  return (
-//    <div className="map">
-//      <Map />
-//    </div>
-//  );
-//}
-
-function MapApp(){
-	return(
-		<div className="MapApp">
-			<Helmet>
-				<script src="./map.js"></script>
-			</Helmet>
-		</div>
-	);
+function MapApp() {
+  return (
+    <div className="MapApp">
+      <Map />
+    </div>
+  );
 }
+
 
 export default MapApp;
 
