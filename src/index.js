75edab549f5f57eff64242e8a33a1a918c6618e4
Kelvin David <kdroomo416@gmail.com>
2020-06-03 12:20:59 +0000

    Attempt to Encapsulate Code #3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MapApp from './mapApp';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MapApp />, document.getElementById('map'));
 
 
---
+++ src/mapApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Map from './map.js'
+import './map.css'
+
+function mapApp() {
+  return (
+    <div className="mapApp">
+      <Map />
+    </div>
+  );
+}
+
+export default MapApp;
+
