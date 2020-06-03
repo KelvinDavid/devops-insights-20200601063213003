e40d41ed2aeb5a0dc20223e5848c30629907aa3c
Kelvin David <kdroomo416@gmail.com>
2020-06-03 13:00:01 +0000

    Fixing Blank page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from './components/AppHeader';
 import AppContainer from './containers/AppContainer';
-import Map from './map.js'
 import './App.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <AppHeader />
       <AppContainer />
-      <Map />
     </div>
   );
 }
