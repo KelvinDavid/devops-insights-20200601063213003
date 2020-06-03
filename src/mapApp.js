4316c7d85f249d6219c41adcdb26fda86126a3d4
Kelvin David <kdroomo416@gmail.com>
2020-06-03 14:08:21 +0000

    Trying Helmet

--- src/mapApp.js
+++ src/mapApp.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Helmet} from "react-helmet";
 import Map from './map.js'
 import './map.css'
 
-function MapApp() {
-  return (
-    <div className="mapApp">
-      <Map />
-    </div>
-  );
+//function MapApp() {
+//  return (
+//    <div className="map">
+//      <Map />
+//    </div>
+//  );
+//}
+
+function MapApp(){
+	return(
+		<div className="MapApp">
+			<Helmet>
+				<script src="./map.js"</script>
+			</Helmet>
+		</div>
+	);
 }
 
 export default MapApp;
