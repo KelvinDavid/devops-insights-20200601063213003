f4d014035c06ea2ba49310f02aa4cb5ce64ef7bd
Kelvin David <kdroomo416@gmail.com>
2020-06-03 23:55:31 +0000

    Syntax

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MapApp from './mapApp';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<MapApp />, document.getElementById('map'));
 
 
