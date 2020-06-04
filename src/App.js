06781c1976972ad7b87fb03d9e267a37f27bd48d
Kelvin David <kdroomo416@gmail.com>
2020-06-04 01:35:55 +0000

    synctasx

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from './components/AppHeader';
 import AppContainer from './containers/AppContainer';
-import MapContainer from './containers/MapContainer'
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-//      <AppHeader />
-//      <AppContainer />
-		<MapContainer / >
+      <AppHeader />
+      <AppContainer />
     </div>
   );
 }
